dab97654ce95176976370880da304bf55430a914
Lucky Arif <230681369+luckyarif@users.noreply.github.com>
2025-09-13 10:38:08 +0530

    All guns charging now.

--- modules/module_handler.py
+++ modules/module_handler.py
@@ -12,37 +12,42 @@ def update_module(gun_id, voltage, current, state):
         assignedModules.module_list_per_gun[gun_id] = [can_id]
         ModuleDataModel.set_module_data[f"MODULE{module_num}"]["VOLTAGE"] = voltage
         ModuleDataModel.set_module_data[f"MODULE{module_num}"]["CURRENT"] = current
-        print(f"Assigned {gun_id} to MODULE{module_num} (CAN_ID={hex(can_id)}) with V={voltage} C={current}")
-        print(f"Assigned Modules: {assignedModules.module_list_per_gun}")
-        print(f"Assigned Modules Values: {ModuleDataModel.set_module_data}")
+        # print(f"Assigned {gun_id} to MODULE{module_num} (CAN_ID={hex(can_id)}) with V={voltage} C={current}")
+        # print(f"Assigned Modules: {assignedModules.module_list_per_gun}")
+        # print(f"Assigned Modules Values: {ModuleDataModel.set_module_data}")
     else:
         assignedModules.module_list_per_gun[gun_id] = []
         module_num = int(gun_id.replace("GUN", ""))
         ModuleDataModel.set_module_data[f"MODULE{module_num}"]["VOLTAGE"] = 0
         ModuleDataModel.set_module_data[f"MODULE{module_num}"]["CURRENT"] = 0
         print(f"Cleared assignment for {gun_id} (inactive or zero demand)")
-        print(f"Assigned Modules: {assignedModules.module_list_per_gun}")
-        print(f"Assigned Modules Values: {ModuleDataModel.set_module_data}")
+        # print(f"Assigned Modules: {assignedModules.module_list_per_gun}")
+        # print(f"Assigned Modules Values: {ModuleDataModel.set_module_data}")
 
 def manage_modules():
     import time
     from modules.message_helper import ModuleMessage as mm
     from modules.constants import assignedModules
+    previous_active_canID = set()
     while True:
-        print("manage_modules thread running...")
+        # print("manage_modules thread running...")
         mm.sync_active_modules()
         # Get all active module CAN IDs
         active_can_ids = set()
         for module_list in assignedModules.module_list_per_gun.values():
             active_can_ids.update(module_list)
 
-        print(f"INFO: Active Module Can IDs: {active_can_ids}")
+        
+        if previous_active_canID != active_can_ids:
+            print(f"INFO: Active Module Can IDs: {active_can_ids}")
+            previous_active_canID = active_can_ids
+        
         # Request voltage and current for each active module
         for can_id in active_can_ids:
             mm.requestModule_Voltage(can_id=can_id)
             mm.requestModule_Current(can_id=can_id)
             mm.requestModule_Temperature(can_id=can_id)
-        time.sleep(1)
+        time.sleep(0.25)
 
 if __name__ == "__main__":
     th1 = threading.Thread(target=update_module)
